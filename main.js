5d971ba4899b73afef258d8038361820ec5edbb8
Isaa Ratzemore <isaacratzemore@gmail.com>
2022-06-02 11:12:02 +1000

    test

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("hello")
+let myAsyncFunction = async () => {
+    let response = await fetch("https://api.hypixel.net/key");
+    let data = await response.json;
+    console.log(data);
+    return data;
+  };
