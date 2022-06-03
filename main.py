e2911b3024652d144ca998ee731d9fe8ae9a63a4
Rodarbal <91778305+Rodarbal@users.noreply.github.com>
2022-06-03 15:41:10 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import mojang
 from directKeys import *
+import requests
+from mojang import MojangAPI
 
-if __name__ == '__main__':
-    click(1024,1024)
+requestlink = str("https://api.hypixel.net/skyblock/bazaar?key=a9408d41-2710-4a17-a642-ccb92f831da6")
+hydata = requests.get(requestlink).json()
+
+def getPearlPrice():
+    ep = float(hydata["products"]["ENDER_PEARL"]["buy_summary"][0]['pricePerUnit'])
+    print(ep)
+    return ep
