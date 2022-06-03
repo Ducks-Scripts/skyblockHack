f95c2e8c8ca4f5ef3afb8f4f87a6c770998cd544
Rodarbal <91778305+Rodarbal@users.noreply.github.com>
2022-06-03 15:41:46 +1000

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-let myAsyncFunction = async () => {
-    let response = await fetch("https://api.hypixel.net/key");
-    let data = await response.json;
-    console.log(data);
-    return data;
-  };
